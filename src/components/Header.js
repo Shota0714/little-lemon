c9b3f75e3c507fe58656f6d4b88f4945a2cdc6ae
Shota0714 <u.shota0714@gmail.com>
2026-05-25 17:05:54 -0400

    Add main layout components and logo for the application

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <header>
+               <meta name="description" content="Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to welcome you to our restaurant!" />
+               <meta name="og:title" content="Little Lemon" />
+               <meta name="og:description" content="Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to welcome you to our restaurant!" />
+               <meta name="og:image" content="https://example.com/little-lemon-image.jpg" />
+            </header>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+function Nav() {
+    return (
+        <>
+            <nav>
+                <img src="logo-little-lemon.png" alt="Little Lemon Logo" />
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Nav;
